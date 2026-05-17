e44cf4e0f3b28db633162168f6954eaf3e5314d1
Azeez Adebayo <debaycisse@gmail.com>
2026-05-17 19:22:51 +0100

    config(settings): add DB parameters and DRF
    Add database parameters to use during connection
    
    Add rest framework to the list of applications

--- 3_countries_data_service/countries_data/settings.py
+++ 3_countries_data_service/countries_data/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
+from os import getenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +79,13 @@ WSGI_APPLICATION = 'countries_data.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': getenv('DB_USERNAME'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'NAME': getenv('DB_NAME'),
+        'HOST': getenv('DB_HOST'),
+        'PORT': getenv('DB_PORT'),
+        'TIMEZONE': 'UTC'
     }
 }
 
